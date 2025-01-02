07a49eb318201dd6c3d0a615eaf0571f02a53e51
ChefZhou <bob3345678@gmail.com>
2025-01-03 01:33:48 +0800

    PostgreSQL增設

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,12 @@
-module.exports = () => ({});
+module.exports = {
+  "users-permissions": {
+    config: {
+      // 使用環境變數，避免硬編碼
+      jwtSecret: process.env.JWT_SECRET,
+      // 建議加入額外的安全設定
+      jwt: {
+        expiresIn: "7d",
+      },
+    },
+  },
+};

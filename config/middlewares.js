af40624fe7e6aaa9602979117be4ac73dec852ef
ChefZhou <bob3345678@gmail.com>
2025-01-02 21:41:14 +0800

    Initial commit for Strapi deployment

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,29 @@
 module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "http:"],
+          "img-src": ["'self'", "data:", "blob:", "http:"],
+          "media-src": ["'self'", "data:", "blob:", "http:"],
+          upgradeInsecureRequests: null,
+        },
+      },
+      cors: {
+        enabled: true,
+        origin: ["http://localhost:3000"],
+      },
+    },
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];

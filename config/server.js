7a11e4ff5160814582d15ed831dcc2105c9a8e9a
ChefZhou <bob3345678@gmail.com>
2025-01-03 01:28:04 +0800

    PostgreSQL增設

--- config/server.js
+++ config/server.js
@@ -1,10 +1,11 @@
+const crypto = require("crypto");
+const key1 = crypto.randomBytes(64).toString("hex");
+const key2 = crypto.randomBytes(64).toString("hex");
+
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
-  },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    keys: [key1, key2], // 使用隨機生成的密鑰
   },
 });

cdda615e9703ffac02bc7e76840f68420c450505
ChefZhou <bob3345678@gmail.com>
2025-01-03 14:52:40 +0800

    新增vercel支援

--- config/middlewares.js
+++ config/middlewares.js
@@ -15,8 +15,9 @@ module.exports = [
       cors: {
         enabled: true,
         origin: [
-          "http://localhost:3000",
-          "https://front-web-profolio.vercel.app/",
+          "http://localhost:3000", // 本地開發環境
+          "https://profolio-frontweb-next-js.vercel.app", // 正式網域
+          "https://profolio-frontweb-next-3e4zhhhg1-zhou-dennys-projects.vercel.app", // 部署預覽網址
         ],
       },
     },
